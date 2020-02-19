9d4b391c3f9e3366866a08766acdbe188c683339
Galata Tona <tonagalata+2@gmail.com>
2020-02-18 20:33:58 -0500

    adds footer component and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 import './App.css';
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
-        
+      <div className="App-outer-container">
+        <Navbar />
+        <div className="App-inner-container">
+        </div>
+        <Footer/>
       </div>
     )
   }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = props => {
+  return (
+    <footer className={styles.footer}>
+      <div>
+      Foodify &copy; 2020
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
