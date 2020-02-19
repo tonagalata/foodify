09437b7c22a0c914f2dd02c9737746bcc921baf6
Galata Tona <tonagalata+2@gmail.com>
2020-02-18 20:05:36 -0500

    adds navbar files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styles from './Navbar.module.css'
 
 const Narbar = props => {
-
+  return(
+    <nav>
+      <h1>Foodify</h1>
+      <ul>
+        <li>Resturants</li>
+        <li>Login</li>
+        <li>Signup</li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Narbar;
