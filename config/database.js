df785f15a844fe85b7b9e810a0c86fb01ed85dfc
Galata Tona <tonagalata+2@gmail.com>
2020-02-22 13:10:18 -0500

    adds mongoDB connection and model

--- config/database.js
+++ config/database.js
@@ -9,5 +9,5 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection;
 
 db.on('connected', () => {
-  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`)
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port} ${db.db.databaseName}`)
 })
