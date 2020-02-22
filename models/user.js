13a279b24faf2685b0ed2c63d07ab31bdf028ffb
Galata Tona <tonagalata+2@gmail.com>
2020-02-22 15:23:30 -0500

    configs password hash

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const SALT_ROUNDS = 6
 
 const Schema = mongoose.Schema;
 
@@ -19,6 +22,16 @@ const userSchema = new Schema({
 
 },{
   timestamps: true
+})
+
+userSchema.pre('save', function(next){
+  const user = this;
+  if(!user.isModified('password')) return next();
+  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash){
+    if(err) return next(err);
+    user.password = hash;
+    next();
+  })
 })
 
 module.exports = mongoose.model('User', userSchema);
