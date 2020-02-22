d8e90e228ece8e3490e11ffa7f4641805aca8868
Galata Tona <tonagalata+2@gmail.com>
2020-02-22 13:09:42 -0500

    adds user model

--- models/user.js
+++ models/user.js
@@ -4,15 +4,21 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: {
-    type: String
+    type: String, 
+    lowercase: true
   },
   email: {
-    type: String
+    type: String, 
+    lowercase: true,
+    required: true,
+    unique: true,
   },
   password: {
     type: String
   }
 
+},{
+  timestamps: true
 })
 
 module.exports = mongoose.model('User', userSchema);
