2fbf2e0e9baad9c2a680485ccd3da2f19450b678
Galata Tona <tonagalata+2@gmail.com>
2020-02-22 13:14:29 -0500

    installed and required bcrypt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const servefavicon = require('serve-favicon')
 const logger = require('morgan')
+const bcrypt = require('bcrypt')
 
 const app = express();
 
